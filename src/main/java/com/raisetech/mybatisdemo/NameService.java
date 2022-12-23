9cf06adda9d950c602517c262ad1e151593fd13b
mako719 <shoki.sm.sk@gmail.com>
2022-12-23 19:50:14 +0900

    @Serviceアノテーションの追加

--- src/main/java/com/raisetech/mybatisdemo/NameService.java
+++ src/main/java/com/raisetech/mybatisdemo/NameService.java
@@ -1,7 +1,6 @@
 package com.raisetech.mybatisdemo;
 
 import java.util.List;
-
 public interface NameService {
     List<Name> findAll();
 }
--- src/main/java/com/raisetech/mybatisdemo/NameServiceImpl.java
+++ src/main/java/com/raisetech/mybatisdemo/NameServiceImpl.java
@@ -1,7 +1,10 @@
 package com.raisetech.mybatisdemo;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class NameServiceImpl implements NameService {
     private final NameMapper nameMapper;
 
